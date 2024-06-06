6dc4b7a3749328df4af266062030d97110516096
rodooldo <rodooldo10@gmail.com>
2024-06-06 18:12:40 +0700

    "SkinEctive Splash and added font"

--- app/src/main/java/com/skinective/MainActivity.kt
+++ app/src/main/java/com/skinective/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.skinective
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val DELAY_MILLIS: Long = 600 // Delay in milliseconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_splashscreen)
+
+        val handler = Handler()
+        handler.postDelayed({
+            // Apply your effect or execute your code here
+            // For example, you can change visibility, animate views, etc.
+            // Example: fadeInEffect()
+        }, DELAY_MILLIS)
+    }
+
+    // Example method for applying an effect
+    private fun fadeInEffect() {
+        // Implement your effect here, for example, fading in a view
+        // Example: viewToAnimate.alpha = 0f; viewToAnimate.animate().alpha(1f).setDuration(1000).start()
     }
 }
